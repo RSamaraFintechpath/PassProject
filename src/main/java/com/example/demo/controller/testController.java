3fc6043b5be2944aea830736a503fbe3a1a1e614
Rami Samara <rsamara@fintechpath.com>
2023-05-09 11:16:01 +0300

    added controller class
    added a controller class with wrong camel case naming convention and added @ApiParam and @ApiOperation to check if the rules are correct

---
+++ src/main/java/com/example/demo/controller/testController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Test")
+public class testController {
+    @GetMapping("/userTest")
+    @ApiParam(value="test", required = true)
+    @ApiOperation(value = "test")
+    public void test(){
+
+    }
+}
