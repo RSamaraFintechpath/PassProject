0d79e214307f03ca4ed9d66b56ff94881947e4df
Rami Samara <rsamara@fintechpath.com>
2023-05-10 10:12:37 +0300

    should be correct

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/testI")
 public class TestController {
     @GetMapping("/userTest/camelCase")
     @ApiParam(value="test", required = true)
