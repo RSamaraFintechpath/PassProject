885a3c9f77101f4a58df6d11c0e3f9c46a3d3f69
Rami Samara <rsamara@fintechpath.com>
2023-05-10 12:57:20 +0300

    should be correct

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +16,13 @@ public class TestController {
     public void test(){
         //impl
     }
+
+    @GetMapping("/userTest/queryTest/{data}")
+    @ApiParam(value="test", required = true)
+    @ApiOperation(value = "test")
+    public String test2(@PathVariable String data){
+        return data;
+    }
+
+
 }
