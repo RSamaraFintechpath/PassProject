f466740ea59dcc939457b9bcb8fe9ae7941b18a8
Rami Samara <rsamara@fintechpath.com>
2023-05-15 11:30:01 +0300

    removed apimodelproperty for test

--- src/main/java/com/example/demo/dto/User.java
+++ src/main/java/com/example/demo/dto/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.dto;
 
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +11,7 @@ import javax.persistence.Entity;
 @Entity
 @Data
 @AllArgsConstructor
+@ApiModel("user")
 public class User {
     @ApiModelProperty(name="userId", position = 1, example = "42354")
     private int userID;
