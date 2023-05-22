1949a1b1947a339cf7c616045fe2a9efe9981bfa
Rami Samara <rsamara@fintechpath.com>
2023-05-22 11:01:21 +0300

    removeed @License and added @Contact for testing

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -14,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
                 title = "My API",
                 version = "1.0",
                 description = "API for managing products",
-                license = @License(
-                        name = "Apache 2.0",
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                contact = @Contact(
+                        name = "John Doe",
+                        email = "johndoe@example.com",
+                        url = "https://example.com"
                 )
         )
 )
