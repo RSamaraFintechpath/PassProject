2431e5d034e963a302b64b78f52e3142044f9590
Rami Samara <rsamara@fintechpath.com>
2023-05-22 11:02:40 +0300

    fixed @SwaggerDefinition to comply with rules

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
                         name = "John Doe",
                         email = "johndoe@example.com",
                         url = "https://example.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
                 )
         )
 )
